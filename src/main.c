965a9dc14bc9020f3e1e57f3949e5171e97d5144
Juan Jose Armando Gerez Jimenez <jjagerezjimenez@gmail.com>
2023-04-28 10:28:42 -0300

    28/4/23 v2 corregir

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stm32f1xx.h>
 
+#define Ciclo_pasada_lazo 8
 
 
 int main(void){
@@ -8,7 +9,7 @@ int main(void){
 // conf C13 como salida
     //GPIOC->CRH = (GPIOC->CHRH & not(GPIO_CRH_CNF13 | GPIO_CRH_MODE13))
     //               | GPIO_CRH_MODE13_1;
-
+//configuro B9 como entrda con pull up
     //GPIOB->CRH = (GPIOB -> CRH & not(GPIO_CRH_CNF9 | GPIO_CRH_CNF9))
     //              | GPIO_CRH_CNF9_1;
     //GPIO->ODR |= GPIO_ODR_ODR9;
@@ -31,7 +32,12 @@ int main(void){
     while(1){                             //Lazo infinito con while            
         //Esperar que se preesione el pulsador
         while((GPIOB->IDR & GPIO_IDR_IDR9));
-        GPIOC->ODR ^=GPIO_ODR_ODR13;
+        GPIOC->ODR |= GPIO_ODR_ODR13;
+        //esperar 1 minuto
+        for (unsigned i=0;i<60000;i++){
+            for (unsigned volatile j;j<((8000000/1000)/Ciclo_pasada_lazo);++j);              //poner los 8Mill en Define mas arriba
+        }
+        GPIOC->ODR &= not
     }
     
    // for(;;){                              //Lazo infinito con for
