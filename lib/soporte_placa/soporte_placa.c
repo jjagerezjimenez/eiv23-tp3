b40d6392d68c94bbcbeefe7e3afaa8dca08e75d5
Juan Jose Armando Gerez Jimenez <jjagerezjimenez@gmail.com>
2023-05-05 10:11:03 -0300

    modificaciones

--- lib/soporte_placa/soporte_placa.c
+++ lib/soporte_placa/soporte_placa.c
@@ -1,3 +1,15 @@
 #include <soporte_placa.h>
+#include <stm32flxx.h>
 
 //implementacion
+
+typedef struct Pin{
+    GPIO_TypeDef * puerto;
+    int nrPin;
+}Pin;
+
+
+Pin const pines[SP_HPIN_LIMITE] 0 {
+    [SP_PB9]={.puerto=GPIOB,.nrPin=9},
+    
+}
