16bff24b7a4e6d60a7ac8c3c6fa4aa832f0f1f5c
Jesus <jesus.avila.05@gmail.com>
2023-05-05 13:41:19 -0300

    Version usando CMSIS, comentada

--- src/main.c
+++ src/main.c
@@ -1,66 +1,36 @@
 #include <stm32f1xx.h>
 
 #define Ciclo_pasada_lazo 13
-
+#define clkkHz 8000 // Frecuencia de reloj en Mhz, 8Mhz 
+#define delay_s 60 // Tiempo de espera en segundos, 1 minuto
+//Notacion de pines: PuertoPin, ej Puerto C Pin 13 C13
 
 int main(void){
-    //Enciende GPIO B y C
-    //RCC->APB2ENR | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOCEN;
-// conf C13 como salida
-    //GPIOC->CRH = (GPIOC->CHRH & not(GPIO_CRH_CNF13 | GPIO_CRH_MODE13))
-    //               | GPIO_CRH_MODE13_1;
-//configuro B9 como entrda con pull up
-    //GPIOB->CRH = (GPIOB -> CRH & not(GPIO_CRH_CNF9 | GPIO_CRH_CNF9))
-    //              | GPIO_CRH_CNF9_1;
-    //GPIO->ODR |= GPIO_ODR_ODR9;
-    unsigned volatileA = 0x80000000U, B=1U<<2, C=1U<<4, D=1U<<7;
-    // bit set
-    A |= B;
-    A |= C;
-    A |= D;
 
-    //bit clear
-    A &= not(B | C);  //ver teclado
-    A &= notC;
-
-    //bit flip
-    A &= D | B;
-
-    A = 0;
-    //Inicia con luz
-    GPIOC->ODR|= GPIO_ODR_ODR13//ver donde colocar este
-    while(1){                             //Lazo infinito con while            
-        //Esperar que se preesione el pulsador
-        while((GPIOB->IDR & GPIO_IDR_IDR9));
-        GPIOC->ODR |= GPIO_ODR_ODR13;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN; //Activa los relojes puertos B y C
+    GPIOC->CRH = (GPIOC -> CRH & ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13))  | GPIO_CRH_MODE13_1; //Configura C13 como salida
+    /*
+    Puerto C CRH MODE13 10, salida a 2 Mhz
+    Pûerto C CRH CNF13  00, salida general Push-Pull
+    */
+    GPIOB->CRH = (GPIOB -> CRH & ~(GPIO_CRH_CNF9  | GPIO_CRH_MODE9 ))  | GPIO_CRH_CNF9_1;   //Configurta B9 como entrada 
+    /*
+    Puerto B CRH MODE9 00, entrada
+    Pûerto B CRH CNF9  10, Entrada con Pull DOWN/UP (Ver ODR)
+    */
+    GPIOB->ODR |= GPIO_ODR_ODR9; // Pone la entrada del pin 9 en modo Pull UP
+    GPIOC->ODR|= GPIO_ODR_ODR13;// ODR13 en 1 ¿Cambia algo?, pone el pin 13 en 1
+    //La luz se enciende al principio, luego al apretar el botòn, permaneces enecendida 1 minuto, para entrar en el modo normal de funcionamiento
+    while(1){                                    
+        //Lazo infinito 
+        while((GPIOB->IDR & GPIO_IDR_IDR9)); // Entramos al comprobar que que el pin 9 està en 1
+        GPIOC->ODR |= GPIO_ODR_ODR13; //Enciende la Luz
         //esperar 1 minuto
-        for (unsigned i=0;i<60000;i++){
-            for (unsigned volatile j;j<((8000000/1000)/Ciclo_pasada_lazo);++j);              //poner los 8Mill en Define mas arriba
+        for (unsigned i=0;i< delay_s ;i++){
+            for (unsigned volatile j;j<((clkkHz)/Ciclo_pasada_lazo);++j);              
         }
-        GPIOC->ODR &= not
-    }
-    
-   // for(;;){                              //Lazo infinito con for
-    //
-  //  }
+        GPIOC->ODR &= ~(GPIO_ODR_ODR13);
+    }   
     return 0;       // x/wx A usar en terminal para ver variable A
 }
 
-//ejemplo en aruino
-#define
-#define
-#define LED_OFF 1
-#define PULSADOR_PULSADO 0
-
-void setup(){
-    pinMose(LED_BUILTIN,OUTPUT);
-    digitalWrite(LED_BUILTIN,LED_OFF);
-    pinMode(PULSADOR,INPUT_PULLUP);
-}
-
-void loop {
-    while(digitalRead(PULSADOR) != PULSADOR_PULSADO);
-    digitalWrite(LED_BUILTIN,LED_ON)
-    delay(60000);
-    digitalWrite(LED_BUILTIN,LED_OFF)
-}
